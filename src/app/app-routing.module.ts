c181b833fff266d5d5398414e72167ba7f0dfb7b
Rillian Diello <rilliansf@gmail.com>
2021-01-23 12:34:08 -0400

    Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardRoutes } from './dashboard';
 
-const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		redirectTo: '/dashboard',
+		pathMatch: 'full'
+	},
+	...DashboardRoutes
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
